2c97cc87326df73f56883a31655319221db60b51
namalfernandolk <namalakit@gmail.com>
2016-08-02 08:41:10 +0530

    First Commit

---
+++ src/main/java/CxfJaxWsClient.java
@@ -0,0 +1,31 @@
+import org.apache.cxf.jaxws.JaxWsProxyFactoryBean;
+
+import net.webservicex.StockQuoteSoap;
+
+public class CxfJaxWsClient {
+
+	public static void main(String[] args) {
+
+		CxfJaxWsClient cxfJaxWsClient = new CxfJaxWsClient();
+		cxfJaxWsClient.invokeJaxWs();
+
+	}
+	
+	private void invokeJaxWs() {
+
+		StockQuoteSoap client = createCXFClient();
+		String response = client.getQuote("test123");
+		System.out.println(response);
+		
+	}
+	
+	protected StockQuoteSoap createCXFClient() {
+	    // we use CXF to create a client for us as its easier than JAXWS and works
+	    JaxWsProxyFactoryBean factory = new JaxWsProxyFactoryBean();
+	    factory.setServiceClass(StockQuoteSoap.class);
+	    factory.setAddress("http://www.webservicex.net/stockquote.asmx");
+	    return (StockQuoteSoap) factory.create();
+	}
+
+
+}
